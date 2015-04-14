0a6cfd26b4c468e38e306b8a2a951abf452084c7
Matthias Hannig <matthias@hannig.cc>
2015-04-14 11:16:59 +0200

    Template rendering

--- app/cli/init.js
+++ app/cli/init.js
@@ -10,8 +10,42 @@
  * (c) 2015 Matthias Hannig
  */
 
+var fs   = require('fs');
+var path = require('path');
+var ejs  = require('ejs');
+
+var logSymbols = require('log-symbols');
+
 var invoiceInit = function(argv) {
-  console.error('Invoice Init: STUB');
+  
+  // Create new invoice json in current working directory.
+  var cwd = process.cwd();
+
+  // Configure invoice.
+  var invoiceConfig = {
+    from: {
+      name: "Ben Utzer",
+      address: ""
+    },
+    to: {
+      address: ""
+    }
+  };
+
+  // Get json template
+  var invoiceJsonTmpl = fs.readFileSync(
+    path.join(__dirname, '../../templates', '_invoice.json.ejs'),
+    { 'encoding': 'utf8' }
+  );
+   
+  // Compile template and render to file
+  var template = ejs.compile(invoiceJsonTmpl);
+  var result   = template(invoiceConfig);
+  console.log(logSymbols.success, 'compiled invoice json template');
+
+  // Check if 
+
+  
 };
 
 // == Export
