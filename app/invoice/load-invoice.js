da45c38b9ebd91890b12d0ea1eda9559f31c51d1
Matthias Hannig <matthias@hannig.cc>
2015-05-01 16:14:16 +0200

    added promise support and added tex template.

---
+++ app/invoice/load-invoice.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * Load raw invoice yaml and parse it.
+ *
+ * (c) 2015 Matthias Hannig
+ */
+
+var promise = require('promise');
+var sym     = require('log-symbols');
+var yaml    = require('js-yaml');
+
+
+var loadInvoice = function(file) {
+
+
+};
+
+// == Expor
+module.exports = loadInvoice;
+
+
